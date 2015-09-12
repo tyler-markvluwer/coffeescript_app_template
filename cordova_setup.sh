f73bf5579c65f3a262826d9c8a2ecd58c43ec9ef
Cam <camherringshaw@gmail.com>
2015-09-12 19:49:03 -0400

    Modifying the initial setup file and package.json

--- cordova_setup.sh
+++ cordova_setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Pre reqs
 # 1. Must have homebrew installed
 # 2. Must have xcode and xcode command line tools installed
@@ -8,33 +9,35 @@
 # install node
 brew update # okay if this fails
 brew install node # don't use sudo on brew commands
-npm install -g grunt-cli
+#npm install -g grunt-cli
 
 # install npm
 # donwload from nodejs site. This is the easiest way
+brew install npm
 
 # install cordova
-sudo npm install -g cordova
+#npm install -g cordova
 
 # install ant
 brew install ant
 
 # install broccoli
-sudo npm install -g broccoli
-sudo npm install -g broccoli-cli
-sudo npm install -g broccoli-coffee
-sudo npm install # install all packages specific to this project
+npm install --save-dev broccoli
+npm install -g broccoli-cli
+#npm install -g broccoli-cli
+#npm install -g broccoli-coffee
+npm install # install all packages specific to this project
 
 # make right directories and all
-mkdir src
-mkdir src/coffee
-mkdir src/sass
+#mkdir src
+#mkdir src/coffee
+#mkdir src/sass
 
-mkdir public
-touch public/index.html
-touch public/style.css
-touch src/coffee/index.coffee
-touch src/sass/index.sass
+#mkdir public
+#touch public/index.html
+#touch public/style.css
+#touch src/coffee/index.coffee
+#touch src/sass/index.sass
 
 # install the coffeescript syntax highlighter for sublime
 # 1. cmd + shift + p
